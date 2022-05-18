7ca35eedeb03e820d0bcfb24a7fbe05b549998cd
Mateusz Daszkiewicz <topdrive@Mini-Mateusz.lan>
2022-05-18 21:59:54 +0100

    Burger works and navigate

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const navBurger = document.querySelector('.nav__menu');
+const navWrapper = document.querySelector('.nav__wrapper');
+const wrappedMenu = document.querySelectorAll('.wrapped_menu > a');
+
 navBurger.addEventListener('click', () => {
-    navBurger.classList.toggle('active');
+    navBurger.classList.toggle('button');
+    navWrapper.classList.toggle('active');
+})
+wrappedMenu.forEach(a =>{
+    a.addEventListener('click', () =>{
+        navBurger.classList.remove('button');
+        navWrapper.classList.remove('active');
+    })
 })
