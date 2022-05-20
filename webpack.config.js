cb0f452fa4730966929c048be99ebda54ec02a23
Mateusz Daszkiewicz <topdrive@Mini-Mateusz.lan>
2022-05-21 00:37:47 +0100

    Mobile finished, need more animations

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
     mode: 'none',
     watch: true,
-    entry: path.resolve(__dirname, 'main.js'),
+    entry:  ['./main.js', './projects.js'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'app.js'
